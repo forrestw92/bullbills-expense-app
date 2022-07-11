0fdf174c2dbf1e10bf3c6ed444a3e5d83547364c
Forrest Walker <forrest.walker@forrestwalker.com>
2022-07-10 21:05:16 -0500

    Added data to header bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,25 @@
 import React from "react";
 import { FiBell, FiSearch } from "react-icons/fi";
+import { BiCreditCard, BiTrendingDown, BiTrendingUp } from "react-icons/bi";
 const Header = () => {
   return (
-    <div className="h-20 w-auto bg-white dark:bg-gray-800 shadow-md rounded flex flex-row px-8">
-      <div className="w-1/3"></div>
+    <div className="h-20 w-auto bg-white dark:bg-gray-800 shadow-md rounded flex flex-row px-8 align-middle items-center">
+      <div className="w-1/3">
+        <div className="flex flex-row items-center align-middle items-center h-16 gap-5">
+          <div className="flex flex-row">
+            <BiTrendingUp className="text-2xl text-green-600" />
+            <span className="text-gray-400 ml-2 text-green-600">$5032</span>
+          </div>
+          <div className="flex flex-row">
+            <BiTrendingDown className="text-2xl text-red-600" />
+            <span className="text-gray-400 ml-2 text-red-600">-$1600</span>
+          </div>
+          <div className="flex flex-row">
+            <BiCreditCard className="text-2xl text-purple-600" />
+            <span className="text-gray-400 ml-2 text-purple-600">9</span>
+          </div>
+        </div>
+      </div>
       <div className="w-1/3">
         <div className="relative align-middle flex my-auto items-center justify-center h-16">
           <input
@@ -20,7 +36,7 @@ const Header = () => {
         </div>
       </div>
       <div className="w-1/3">
-        <div className="w-full h-full flex items-center justify-end">
+        <div className="w-full h-full flex items-center justify-end h-16">
           <div className="relative">
             <button type="button" id="notification-menu">
               <FiBell
