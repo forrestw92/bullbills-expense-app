832d55b9796913a8ff965971f2417aeb5bb8f8f3
Forrest Walker <forrest.walker@forrestwalker.com>
2022-07-11 19:33:03 -0500

    Responsive grid

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -56,8 +56,8 @@ const Home = () => {
 
   return (
     <div className="flex flex-col w-full text-gray-400">
-      <div className="grid grid-cols-3 grid-rows-3 gap-5">
-        <div className="col-span-1 bg-slate-800 rounded-md shadow-xl transition-all hover:scale-105">
+      <div className="grid grid-cols-3 grid-rows-4 gap-5">
+        <div className="col-span-3 lg:col-span-1  bg-slate-800 rounded-md shadow-xl transition-all hover:scale-105">
           <div className="p-10 flex flex-col flex-nowrap h-full w-full ">
             <h1 className="text-gray-300 text-2xl">Hello John,</h1>
             <p className="text-gray-400">
@@ -69,7 +69,7 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="relative col-span-1 bg-slate-800 rounded-md shadow-xl transition-all hover:scale-105">
+        <div className="relative col-span-3 md:col-span-1 lg:col-span-1 bg-slate-800 rounded-md shadow-xl transition-all hover:scale-105">
           <BiTrendingUp className="text-green-300 h-16 w-16 w-fit justify-center   rounded m-auto mt-10" />
           <div className=" flex flex-col flex-nowrap h-full w-full pt-5 pb-10">
             <h1 className="text-gray-300 text-4xl text-center text-green-300 ">
@@ -78,7 +78,7 @@ const Home = () => {
             <p className="text-center pt-4 text-gray-500">Income</p>
           </div>
         </div>
-        <div className="relative col-span-1 bg-slate-800 rounded-md shadow-xl transition-all hover:scale-105">
+        <div className="relative col-span-3 md:col-span-1 lg:col-span-1 bg-slate-800 rounded-md shadow-xl transition-all hover:scale-105">
           <BiTrendingDown className="text-red-300 h-16 w-16 w-fit justify-center   rounded m-auto mt-10" />
           <div className=" flex flex-col flex-nowrap h-full w-full pt-5 pb-10">
             <h1 className="text-gray-300 text-4xl text-center text-red-300 ">
@@ -87,7 +87,7 @@ const Home = () => {
             <p className="text-center pt-4 text-gray-500">Expenses</p>
           </div>
         </div>
-        <div className="col-span-2 row-span-2 bg-purple-900 rounded-md shadow-xl p-5 shadow-inner shadow-2xl shadow-slate-900">
+        <div className="col-span-3 row-span-4 lg:col-span-2 lg:row-span-2  lg:h-auto bg-purple-900 rounded-md shadow-xl p-5 shadow-inner shadow-2xl shadow-slate-900">
           <div className=" h-full fill-blue-700">
             <BarChart />
           </div>
